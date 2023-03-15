ec0894c46464e34d2f50dea59b2220b79c290e73
Vadimkandeev <vadimkandeev@gmail.com>
2023-03-15 13:46:14 +0400

    Last update_15_03

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import time
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.implicitly_wait(4)
+    browser.maximize_window()
+    yield browser
+
+    browser.quit()
--- simple_test.py
+++ simple_test.py
@@ -1,2 +1,19 @@
-def test_just():
-    assert 1 == 2
+from time import sleep
+from page.AuthPage import AuthPage
+from page.MainPage import MainPage
+
+
+def authorization_test(browser):
+    email = "vadimkandeev@gmail.com"
+    auth_page = AuthPage(browser)
+    auth_page.go()
+    auth_page.login_as(email, "1475Maximus2705")
+
+    main_page = MainPage(browser)
+    main_page.open_menu()
+    info = main_page.get_account_info()
+
+    assert main_page.get_current_url().endswith("vadimkandeev_1975/boards")
+    assert info[0] == "Vadimkandeev"
+    assert info[1] == email
+    
